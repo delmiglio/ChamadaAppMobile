8091fd7e34dcab6beca29b691146eb69bd67314a
Daniel <Daniel@DANIEL>
2016-05-20 22:11:42 -0300

    Implementação e verificação de chamadas existentes a serem respondidas.

--- ChamadaAppMobile/ChamadaAppMobile/ChamadaAppMobile/Forms/ContentPageHome.cs
+++ ChamadaAppMobile/ChamadaAppMobile/ChamadaAppMobile/Forms/ContentPageHome.cs
@@ -1,10 +1,14 @@
-﻿using ChamadaApp.Domain.VO;
+﻿using ChamadaApp.Api.Utils;
+using ChamadaApp.Domain.VO;
+using ChamadaAppMobile.Services;
+using ChamadaAppMobile.Utils;
+using ChamadaAppMobile.VO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ChamadaAppMobile.Forms
@@ -12,7 +16,8 @@ namespace ChamadaAppMobile.Forms
     public class ContentPageHome : ContentPage
     {
         UsuarioVO usuario;
-
+        ChamadaForPresencaVO chamada;
+        
         public ContentPageHome()
         {
             InicializarUsuario();
@@ -29,6 +34,16 @@ namespace ChamadaAppMobile.Forms
                     FontAttributes = FontAttributes.Bold,
                     TextColor = Color.White
                 }
+            };
+
+            ContentView dadosChamada = new ContentView
+            {
+                BackgroundColor = Color.FromHex("434E4E"),
+                Padding = new Thickness(20, 15),
+                Margin = new Thickness(0, 20),
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Fill,
+                IsVisible = false
             };
 
             ContentView footer = new ContentView
@@ -62,7 +77,8 @@ namespace ChamadaAppMobile.Forms
 
                 Children =
                 {
-                    lbUsuario
+                    lbUsuario,
+                    dadosChamada
                 },
 
                 Padding = new Thickness(20)
@@ -79,11 +95,63 @@ namespace ChamadaAppMobile.Forms
             };
 
             this.BackgroundColor = Color.White;
+
+            GetMateriaChamada(dadosChamada);
         }
 
         private void InicializarUsuario()
         {
             usuario = App.DataBase.GetUniqueUser();
+        }
+
+        private void GetMateriaChamada(ContentView view)
+        {
+            Label lb = new Label
+            {                
+                FontSize = 20,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.White
+            };
+
+            GetRest getChamada = new GetRest();
+
+            string parametros = string.Format("alunoId={0}", 3);//usuario.Id);
+
+            getChamada.GetResponse<Retorno>("chamada", parametros).ContinueWith(t =>
+            {                
+                if(t.IsCompleted)
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        Retorno obj;                       
+
+                        if (t.Result is Retorno)
+                        {
+                            obj = (Retorno)t.Result;
+
+                            if ((TpRetornoEnum)obj.TpRetorno == TpRetornoEnum.Sucesso && obj.ObjRetorno != null)
+                            {
+                                if (obj.ObjTypeName == typeof(ChamadaForPresencaVO).Name)
+                                {
+                                    chamada = Metodos.JsonToCustomObject<ChamadaForPresencaVO>(obj.ObjRetorno);
+                                }
+
+                                view.BackgroundColor = Color.FromHex("328325");                                                  
+                            }
+                            else if((TpRetornoEnum)obj.TpRetorno == TpRetornoEnum.Erro)
+                            {
+                                view.BackgroundColor = Color.FromHex("A63030");                                
+                            }
+
+                            lb.Text = obj.RetornoMensagem + ((!string.IsNullOrWhiteSpace(obj.RetornoDescricao)) ? 
+                                                                (Environment.NewLine + obj.RetornoDescricao) : "");
+                            view.IsVisible = true;
+                            view.Content = lb;
+                        }
+                    });
+                }
+            });
         }
     }
 }
---
+++ ChamadaAppMobile/ChamadaAppMobile/ChamadaAppMobile/Utils/VO/ChamadaForPresencaVO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadaAppMobile.VO
+{
+    public class ChamadaForPresencaVO
+    {
+        public ChamadaForPresencaVO()
+        {
+
+        }        
+
+        public int Id { get; set; }
+
+        public int AlunoId { get; set; }
+
+        public string Situacao { get; set; }
+
+        public string Materia { get; set; }
+
+        public string Professor { get; set; }
+    }
+}
