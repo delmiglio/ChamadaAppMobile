851540ecf0281c744565a7517bd5df8f28722276
Daniel <Daniel@DANIEL>
2016-05-21 14:35:42 -0300

    Mudando o diretorio

--- ChamadaAppMobile/ChamadaAppMobile/ChamadaAppMobile.Droid/Resources/Resource.Designer.cs
+++ ChamadaAppMobile/ChamadaAppMobile/ChamadaAppMobile.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace ChamadaAppMobile.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::ChamadaAppMobile.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Attribute
